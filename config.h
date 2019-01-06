34e944706194be42d2c56f4bdb06d025c004cb15
ZhangChunyang <zcyhun@gmail.com>
2019-01-06 22:17:22 +0800

    irtest

---
+++ config.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <stc12.h>
+
+#define IRPIN=P1_5
---
+++ irtest/rftest.c
@@ -0,0 +1,7 @@
+#include "../config.h"
+uint8_t buffer[100]; // 红外电平宽度测量，一个宽度占用两字节
+					// 低电平开始，长高电平结束
+void main(void){
+	
+
+}
